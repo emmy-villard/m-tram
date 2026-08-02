e445e16c86d8b557833bd611cb84e276fe7ee9e5
Emmy <emmy.villard.pro@proton.me>
2026-08-02 10:42:04 +0200

    refactor: shorten submodule names

--- src/etl_trr.py
+++ src/etl_trr.py
@@ -1,5 +1,5 @@
-from extract.fetch_data_trr import fetch_trr
-from transform.transform_trr import raw_to_pandas_trr
+from extract.trr import fetch_trr
+from transform.trr import raw_to_pandas_trr
 
 raw_data = fetch_trr()
 dataframe = raw_to_pandas_trr(raw_data)

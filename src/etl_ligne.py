65b13b692c928762d44b4a6bafff01af17669cac
Emmy VILLARD <emmy.villard.pro@proton.me>
2026-08-05 12:43:55 +0200

    feat: regroup dynamic mdata loading logic

--- src/etl_ligne.py
+++ src/etl_ligne.py
@@ -1,11 +1,11 @@
 from extract.ligne import fetch_ligne
 from transform.ligne import raw_to_pandas_ligne
-from load.ligne import load_ligne
+from load.mdata_dyn import load_table
 
 def etl_ligne():
     raw_data = fetch_ligne()
     dataframe = raw_to_pandas_ligne(raw_data)
-    load_ligne(dataframe)
+    load_table(dataframe, "ligne")
 
 if __name__ == "__main__":
     etl_ligne()
---
+++ src/load/mdata_dyn.py
@@ -0,0 +1,34 @@
+from sqlalchemy import MetaData, PrimaryKeyConstraint
+from sqlalchemy import Table, Column, Integer, Text, select
+from sqlalchemy.dialects.postgresql import insert
+from .engine import engine
+
+def load_table(dataframe, table_name):
+    with engine.connect() as conn:
+        table, new_table_data = getTables(table_name)
+        dataframe.to_sql(f"new_{table_name}_data", conn, if_exists="replace")
+        lines_with_data = select(new_table_data).where(new_table_data.c[f"{table_name}_nsv_id"] != 0)
+        insert_new_lines = insert(table).from_select(
+            [f"{table_name}_id", f"{table_name}_time", f"{table_name}_nsv_id"], lines_with_data)
+        upsert_new_lines = insert_new_lines.on_conflict_do_nothing()
+        conn.execute(upsert_new_lines)
+        conn.commit()
+
+def getTables(table_name):
+    table = Table(
+        table_name,
+        MetaData(),
+        Column(f"{table_name}_id", Integer),
+        Column(f"{table_name}_time", Text),
+        Column(f"{table_name}_nsv_id", Integer),
+        PrimaryKeyConstraint(f"{table_name}_id", f"{table_name}_time"),
+    )
+    new_table_data = Table(
+        f"new_{table_name}_data",
+        MetaData(),
+        Column(f"{table_name}_id", Integer),
+        Column(f"{table_name}_time", Text),
+        Column(f"{table_name}_nsv_id", Integer),
+        PrimaryKeyConstraint(f"{table_name}_id", f"{table_name}_time"),
+    )
+    return table, new_table_data

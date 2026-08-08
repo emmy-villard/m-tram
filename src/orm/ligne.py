29f64554d06817297b054fe0d6f7aa2076b1699f
Emmy VILLARD <emmy.villard.pro@proton.me>
2026-08-08 18:27:37 +0200

    docs: add documentation for orm & orchestration modules

--- src/orm/ligne.py
+++ src/orm/ligne.py
@@ -2,6 +2,19 @@ from sqlalchemy import Column, Text, TIMESTAMP, Integer, PrimaryKeyConstraint
 from orm.base import Base
 
 class Ligne(Base):
+    """
+    Class definition of table : "ligne". Used to store long-term "ligne" data about tram traffic conditions in Grenoble
+
+    Properties
+    ----------
+    
+    ligne_id : sqlalchemy.Column(sqlalchemy.Text)
+        tram line
+    ligne_id : sqlalchemy.Column(sqlalchemy.TIMESTAMP)
+        report timestamp
+    ligne_nsv_id : sqlalchemy.Column(sqlalchemy.Integer)
+        traffic condition
+    """
     __tablename__ = "ligne"
 
     ligne_id = Column(Text)
@@ -13,6 +26,19 @@ class Ligne(Base):
         return f"ligne(ligne_id={self.ligne_id}, ligne_time={self.ligne_time}, ligne_nsv_id={self.ligne_nsv_id})"
 
 class NewLigneData(Base):
+    """
+    Class definition of table : "new_ligne_data". Used for temporary storage
+
+    Properties
+    ----------
+    
+    ligne_id : sqlalchemy.Column(sqlalchemy.Text)
+        tram line
+    ligne_id : sqlalchemy.Column(sqlalchemy.TIMESTAMP)
+        report timestamp
+    ligne_nsv_id : sqlalchemy.Column(sqlalchemy.Integer)
+        traffic condition
+    """
     __tablename__ = "new_ligne_data"
 
     ligne_id = Column(Text)

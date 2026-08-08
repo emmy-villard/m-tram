5811467b7b8b4a5b9e7856b4e6253625b15c614f
Emmy VILLARD <emmy.villard.pro@proton.me>
2026-08-08 14:06:40 +0200

    docs: add basic python documentation

--- src/db_connection/engine.py
+++ src/db_connection/engine.py
@@ -1,6 +1,14 @@
 from sqlalchemy import URL, create_engine
 import os
 
+"""
+File for the engine Singleton
+"""
+
+
+"""
+SQLAlchemy database url
+"""
 url = URL.create(
     drivername="postgresql+psycopg2",
     username=os.getenv('POSTGRES_USER'),
@@ -9,4 +17,8 @@ url = URL.create(
     database=os.getenv('POSTGRES_DB'),
     password=os.getenv('POSTGRES_PASSWORD'),
 )
+
+"""
+SQLAlchemy database engine
+"""
 engine = create_engine(url)
--- src/etl/main.py
+++ src/etl/main.py
@@ -2,6 +2,15 @@ from etl.etl_trr import etl_trr
 from etl.etl_ligne import etl_ligne
 
 def main():
+    """
+    Launches ETL procedure for data : "trr", "ligne"
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    """
     etl_trr()
     etl_ligne()
 

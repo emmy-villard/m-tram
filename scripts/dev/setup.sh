5f53d321b800a92f1830e5128f5575635a319eb3
Emmy VILLARD <emmy.villard.pro@proton.me>
2026-08-09 08:48:51 +0200

    reafactor: improve script split

--- scripts/dev/setup.sh
+++ scripts/dev/setup.sh
@@ -1,13 +1,4 @@
-POSTGRES_USER=app
-POSTGRES_PASSWORD=$(tr -dc 'A-Za-z0-9!?%=' < /dev/urandom | head -c 32)
-POSTGRES_PORT=5433
-POSTGRES_DB=api_db
-
-echo "POSTGRES_USER=$POSTGRES_USER" > .env
-echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> .env
-echo "POSTGRES_PORT=$POSTGRES_PORT" >> .env
-echo "POSTGRES_DB=$POSTGRES_DB" >> .env
-
+./scripts/generate.env.sh
 python3 -m pip install -e .
 source scripts/dev/entrypoint.sh
 

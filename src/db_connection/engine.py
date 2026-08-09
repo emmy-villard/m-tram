feec2d699c91999dee00f551d268a6d214b036a2
Emmy VILLARD <emmy.villard.pro@proton.me>
2026-08-09 08:50:59 +0200

    feat: raise ValueErreor when database env var empty

--- src/db_connection/engine.py
+++ src/db_connection/engine.py
@@ -1,12 +1,18 @@
 from sqlalchemy import URL, create_engine
 import os
 
+def _get_env_var(name):
+    var=os.getenv(name)
+    if not var:
+        raise ValueError(f"Environement variable {name} is empty")
+    return var
+
 url = URL.create(
     drivername="postgresql+psycopg2",
-    username=os.getenv('POSTGRES_USER'),
+    username=_get_env_var('POSTGRES_USER'),
     host="localhost",
-    port=os.getenv('POSTGRES_PORT'),
-    database=os.getenv('POSTGRES_DB'),
-    password=os.getenv('POSTGRES_PASSWORD'),
+    port=_get_env_var('POSTGRES_PORT'),
+    database=_get_env_var('POSTGRES_DB'),
+    password=_get_env_var('POSTGRES_PASSWORD'),
 )
 engine = create_engine(url)

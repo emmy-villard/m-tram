a362bfbb36298f8d6e185b98bd29f8ca23ccf514
Emmy VILLARD <emmy.villard.pro@proton.me>
2026-08-16 17:59:14 +0200

    chore: rename local docker-compose file

--- scripts/dev/build_docker_img.sh
+++ scripts/dev/build_docker_img.sh
@@ -1,1 +1,1 @@
-docker build -f Docker/Dockerfile.etl -t mtram-etl:0.1 .
+docker build -f Docker/Dockerfile.etl -t mtram-etl:0.0.1 .
--- scripts/dev/entrypoint.sh
+++ scripts/dev/entrypoint.sh
@@ -2,5 +2,5 @@
 
 source scripts/setup_env_var.sh
 
-docker compose -f dev_tools/docker-compose-local-db.yml --env-file .env down -v
-docker compose -f dev_tools/docker-compose-local-db.yml --env-file .env up -d --force-recreate --wait
+docker compose -f dev_tools/docker-compose-local.yml --env-file .env down -v
+docker compose -f dev_tools/docker-compose-local.yml --env-file .env up -d --force-recreate --wait

66d66801d32eef73e93a2336246fb520cba3f094
Emmy <emmy.villard.pro@proton.me>
2026-08-26 11:01:29 +0200

    tests: change test setup

---
+++ scripts/tests/generate_export.env.sh
@@ -0,0 +1,22 @@
+write_variables() {
+    # Test db variables
+    POSTGRES_TEST_USER=test
+    POSTGRES_TEST_PASSWORD=$(tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 32)
+    POSTGRES_TEST_PORT=54320
+    POSTGRES_TEST_HOST=localhost
+    POSTGRES_TEST_DB=test_db
+    echo "POSTGRES_TEST_USER=$POSTGRES_TEST_USER" >> .env.test
+    echo "POSTGRES_TEST_PASSWORD=$POSTGRES_TEST_PASSWORD" >> .env.test
+    echo "POSTGRES_TEST_PORT=$POSTGRES_TEST_PORT" >> .env.test
+    echo "POSTGRES_TEST_HOST=$POSTGRES_TEST_HOST" >> .env.test
+    echo "POSTGRES_TEST_DB=$POSTGRES_TEST_DB" >> .env.test
+}
+
+export_variables() {
+    set -a
+    source .env.test
+    set +a
+}
+
+write_variables
+export_variables
---
+++ scripts/tests/setup-test-env.sh
@@ -0,0 +1,5 @@
+pip3 install -e .
+source scripts/tests/generate_export.env.sh
+
+docker compose -f scripts/tests/docker-compose-test-db.yml --env-file .env.test down -v
+docker compose -f scripts/tests/docker-compose-test-db.yml --env-file .env.test up -d --force-recreate --wait

cf4e167c65cea7f0484ea3fed0b622dd69ff822a
Emmy <emmy.villard.pro@proton.me>
2026-09-08 12:49:03 +0200

    fix: exec etl_meteo dag in dag_tests

--- tests/airflow/dag_tests.sh
+++ tests/airflow/dag_tests.sh
@@ -2,6 +2,7 @@ export AIRFLOW_API=$(docker ps --format '{{.Names}}' | grep airflow-apiserver)
 export APP_DB=$(docker ps --format '{{.Names}}' | grep "\-db")
 docker exec $AIRFLOW_API airflow dags test etl_trr
 docker exec $AIRFLOW_API airflow dags test etl_ligne
+docker exec $AIRFLOW_API airflow dags test etl_meteo
 export request_trr="SELECT EXISTS (SELECT 1 FROM trr LIMIT 1);"
 export request_ligne="SELECT EXISTS (SELECT 1 FROM ligne LIMIT 1);"
 export request_openmeteo="SELECT EXISTS (SELECT 1 FROM openmeteo LIMIT 1);"

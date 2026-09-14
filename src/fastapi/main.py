73238c89b6f6de1676cf1fbd0ce66ed70c1f5c8f
Emmy <emmy.villard.pro@proton.me>
2026-09-14 20:40:07 +0200

    feat: start implementation of fastapi

--- src/fastapi/main.py
+++ src/fastapi/main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
+from db_connection.engine import engine
+from sqlalchemy.orm import Session
+from sqlalchemy import text, func, select
+from orm.ligne import Ligne
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/raw/ligne")
+def get_line_number():
+    with Session(engine) as session:
+        select_stmt = select(Ligne)
+        lines = session.execute(select_stmt).scalars().all()
+        return {
+            "result": [
+                {
+                    "ligne_id": line.ligne_id,
+                    "ligne_time": line.ligne_time,
+                    "ligne_nsv_id": line.ligne_nsv_id,
+                }
+                for line in lines
+            ]
+        }

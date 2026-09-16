00ff498302abc7cd8992bd9d18801ef0998da203
Emmy <emmy.villard.pro@proton.me>
2026-09-16 18:54:06 +0200

    chore: remove airflow test scripts

--- scripts/tests/setup-airflow-test-env.sh
+++
@@ -1,5 +0,0 @@
-pip3 install -e .[dev]
-source scripts/generate_export.env.sh
-
-docker compose -f scripts/tests/docker-compose-airflow-tests.yml down -v --remove-orphans
-docker compose -f scripts/tests/docker-compose-airflow-tests.yml up -d --force-recreate --wait

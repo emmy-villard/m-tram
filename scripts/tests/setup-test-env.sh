d973294aac5fdfecc7dd4e319847da28e3ad79b9
Emmy <emmy.villard.pro@proton.me>
2026-09-16 19:57:20 +0200

    fix: unit tests

--- scripts/tests/setup-test-env.sh
+++ scripts/tests/setup-test-env.sh
@@ -1,8 +1,8 @@
 pip3 install -e .[dev]
 source scripts/tests/generate_export.env.sh
 
-docker compose -f scripts/tests/docker-compose-test-db.yml --env-file .env.test down -v --remove-orphans
-docker compose -f scripts/tests/docker-compose-test-db.yml --env-file .env.test up -d --force-recreate --wait
-
 docker compose -f scripts/tests/docker-compose-test-fastapi.yml --env-file .env.test down -v --remove-orphans
+docker compose -f scripts/tests/docker-compose-test-db.yml --env-file .env.test down -v --remove-orphans
+
+docker compose -f scripts/tests/docker-compose-test-db.yml --env-file .env.test up -d --force-recreate --wait
 docker compose -f scripts/tests/docker-compose-test-fastapi.yml --env-file .env.test up -d --force-recreate --wait

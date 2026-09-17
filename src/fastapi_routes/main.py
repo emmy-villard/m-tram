f7151136f0e54190656d16a928d5de6692006e18
Emmy <emmy.villard.pro@proton.me>
2026-09-17 20:48:35 +0200

    refactor: generalized raw data serving with FastAPI

--- src/fastapi_routes/main.py
+++ src/fastapi_routes/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
-from fastapi_routes.requests import trr
-from fastapi_routes.requests import atmo, ligne, openmeteo
-from fastapi_routes.requests import count
+from fastapi_routes.requests import raw, count
+
+from orm.ligne import Ligne 
+from orm.trr import Trr
+from orm.openmeteo import OpenMeto
+from orm.atmo import Atmo
 
 app = FastAPI()
-
 
 @app.get("/count")
 def get_count():
@@ -12,18 +14,18 @@ def get_count():
 
 @app.get("/raw/ligne")
 def get_raw_line():
-    return ligne.get_raw_data()
+    return raw.get_data(Ligne)
 
 @app.get("/raw/trr")
 def get_raw_trr():
-    return trr.get_raw_data()
+    return raw.get_data(Trr)
 
 @app.get("/raw/atmo")
 def get_raw_atmo():
-    return atmo.get_raw_data()
+    return raw.get_data(OpenMeto)
 
 @app.get("/raw/openmeteo")
 def get_raw_openmeteo():
-    return openmeteo.get_raw_data()
+    return raw.get_data(Atmo)
 
 

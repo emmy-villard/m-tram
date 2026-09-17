326d7d53dc5f79730a0e6f2c9d58fe203b73676d
Emmy <emmy.villard.pro@proton.me>
2026-09-17 20:20:29 +0200

    chore: add alembic upgrade commande to setup-test-env.sh

--- scripts/tests/setup-test-env.sh
+++ scripts/tests/setup-test-env.sh
@@ -4,3 +4,5 @@ source scripts/tests/generate_export.env.sh
 docker compose -f scripts/tests/docker-compose-test.yml --env-file .env.test down -v --remove-orphans
 docker compose -f scripts/tests/docker-compose-test.yml --env-file .env.test build
 docker compose -f scripts/tests/docker-compose-test.yml --env-file .env.test up -d --force-recreate --wait
+
+alembic upgrade head

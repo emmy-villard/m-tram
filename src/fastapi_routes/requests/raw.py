6eff45889293a0ee988cb55c8e091f5312f2f461
Emmy <emmy.villard.pro@proton.me>
2026-09-17 21:20:02 +0200

    chore: remove unused import

--- src/fastapi_routes/requests/raw.py
+++ src/fastapi_routes/requests/raw.py
@@ -1,8 +1,6 @@
 from sqlalchemy import select
 from sqlalchemy.orm import Session, DeclarativeBase
-
 from db_connection.engine import engine
-from orm.atmo import Atmo
 
 def get_data(table: type[DeclarativeBase]):
     with Session(engine) as session:

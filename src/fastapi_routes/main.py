5d2dadafbe215a3f50b90cdc8d6b9a80b6c6388b
Emmy <emmy.villard.pro@proton.me>
2026-09-18 09:03:56 +0200

    feat: add GET / path to FastAPI

--- src/fastapi_routes/main.py
+++ src/fastapi_routes/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from fastapi_routes.requests import raw, count
 
 from orm.ligne import Ligne 
@@ -6,7 +7,21 @@ from orm.trr import Trr
 from orm.openmeteo import OpenMeto
 from orm.atmo import Atmo
 
+import os
+from markdown import markdown
+
 app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+def get_api_doc():
+    api_doc_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "docs", "api.md"
+    )
+    with open(api_doc_path) as file:
+        content = file.read()
+        html = markdown(content)
+        return html
 
 @app.get("/count")
 def get_count():
